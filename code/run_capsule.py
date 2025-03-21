63b90116af935982a3619750e5924dfa931b8987
houhan@gmail.com <han.hou@alleninstitute.org>
2025-03-21 20:44:44 +0000

    always output log file

--- code/run_capsule.py
+++ code/run_capsule.py
@@ -28,7 +28,7 @@ SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
 
 logging.basicConfig(level=logging.INFO, 
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    handlers=[# logging.FileHandler(f'{SCRIPT_DIR}/../results/run.log'),  # Remove logging file to avoid pipeline conflict
+                    handlers=[logging.FileHandler(f'{SCRIPT_DIR}/../results/job_wrapper.log'),   # always output log file
                               logging.StreamHandler()])
 logger = logging.getLogger()  # Use root logger to capture all logs (including logs from imported modules)
 
